285a197356eb043921c27cefd49c24c2bdbfa488
Aitor Moreno <aitor@rojo2.com>
2023-04-04 17:26:46 +0200

    fix: use color picker constants from enum ColorMode

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -12,15 +12,15 @@
       <CombinedColorPicker :hue="combinedHue" v-model="current" />
       <HuePicker v-model="combinedHue" />
       <TabMenu>
-        <Tab :active="mode === 'hex'" @click="mode = 'hex'">Hex</Tab>
-        <Tab :active="mode === 'hsl'" @click="mode = 'hsl'">HSL</Tab>
-        <Tab :active="mode === 'rgb'" @click="mode = 'rgb'">RGB</Tab>
-        <Tab :active="mode === 'palette'" @click="mode = 'palette'">Palette</Tab>
+        <Tab :active="mode === ColorMode.HEX" @click="mode = ColorMode.HEX">Hex</Tab>
+        <Tab :active="mode === ColorMode.HSL" @click="mode = ColorMode.HSL">HSL</Tab>
+        <Tab :active="mode === ColorMode.RGB" @click="mode = ColorMode.RGB">RGB</Tab>
+        <Tab :active="mode === ColorMode.PALETTE" @click="mode = ColorMode.PALETTE">Palette</Tab>
       </TabMenu>
-      <ColorHex v-if="mode === 'hex'" v-model="current" />
-      <ColorHSL v-else-if="mode === 'hsl'" v-model="current" />
-      <ColorRGB v-else-if="mode === 'rgb'" v-model="current" />
-      <Palette v-else-if="mode === 'palette'" :selected-color="previous" :palette="document.palette" @select="onSelectColor" />
+      <ColorHex v-if="mode === ColorMode.HEX" v-model="current" />
+      <ColorHSL v-else-if="mode === ColorMode.HSL" v-model="current" />
+      <ColorRGB v-else-if="mode === ColorMode.RGB" v-model="current" />
+      <Palette v-else-if="mode === ColorMode.PALETTE" :selected-color="previous" :palette="document.palette" @select="onSelectColor" />
     </div>
     <Button label="Accept" @click="onOk">
       Accept
@@ -37,6 +37,7 @@ import CombinedColorPicker from '@components/CombinedColorPicker.vue'
 import ColorHex from '@components/ColorHex.vue'
 import ColorHSL from '@components/ColorHSL.vue'
 import ColorRGB from '@components/ColorRGB.vue'
+import ColorMode from '@/enums/ColorMode'
 import Palette from '@components/Palette.vue'
 import Button from '@components/Button.vue'
 import Dropdown from '@components/Dropdown.vue'
@@ -50,7 +51,7 @@ const current = ref(document.color)
 
 const parsedColor = ref(Color.parse(current.value))
 
-const mode = ref('hex')
+const mode = ref(ColorMode.HEX)
 const hue = ref(Color.hue(parsedColor.value))
 const combinedHue = ref(hue)
 const hexColor = ref(Color.stringify(parsedColor.value, 'hex').slice(1, 7))
---
+++ src/enums/ColorMode.js
@@ -0,0 +1,8 @@
+export const ColorTab = {
+  HEX: 'hex',
+  HSL: 'hsl',
+  RGB: 'rgb',
+  PALETTE: 'palette'
+}
+
+export default ColorTab
