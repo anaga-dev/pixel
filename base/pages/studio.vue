f8870110f87a8b3c94c7a2633d2ae1a6b23ae51c
Omar Moreno Melcón <omar@anaga.dev>
2023-09-15 16:39:32 +0200

    style: change overlay color and get rid of pixel filter

--- base/pages/studio.vue
+++ base/pages/studio.vue
@@ -3,7 +3,3 @@
     <Studio />
   </ClientOnly>
 </template>
-
-<script setup>
-import Studio from '@/components/Studio.client.vue'
-</script>
--- components/Overlay.vue
+++ components/Overlay.vue
@@ -1,43 +1,14 @@
 <template>
   <div class="Overlay">
   </div>
-  <svg>
-    <filter id="pixelate">
-      <feFlood x="4" y="4" height="2" width="2" flood-color="black"/>
-      
-      <feComposite width="8" height="8"/>
-      
-      <feTile result="a"/>
-      
-      <feComposite in="SourceGraphic" in2="a" 
-                  operator="in"/>
-      
-      <feMorphology operator="dilate"
-                    radius="4"/>
-    </filter>
-  </svg>
 </template>
 
 <style scoped>
 .Overlay {
   position: fixed;
   inset: 0;
-  backdrop-filter: url('#pixelate');
   background-color: var(--colorOverlay);
   z-index: 999;
-  animation: pixelate 3s infinite alternate;
-}
-
-@keyframes pixelate {
-  from {
-    --rad: 0;
-  } to {
-    --rad: 4;
-  }
-}
-
-#pixelate feMorphology {
-  radius: var(--radius);
 }
 </style>
 
