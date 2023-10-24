89b2b065604a255bfa1002bd12acbd8129409c5d
AzazelN28 <asakon28@gmail.com>
2023-10-24 23:44:08 +0200

    refactor: color picker render and handling

--- components/CombinedColorPicker.vue
+++ components/CombinedColorPicker.vue
@@ -3,8 +3,6 @@
     <canvas
       ref="canvas"
       @pointerdown="startDragging"
-      @pointermove="updateColor"
-      @pointerup="stopDragging"
       draggable="false"
     ></canvas>
     <div class="sample" :style="style"></div>
@@ -14,6 +12,7 @@
 <script setup>
 import Canvas from '@/pixel/canvas/Canvas'
 import Color from '@/pixel/color/Color'
+import CombinedColorPicker from '@/pixel/canvas/CombinedColorPicker'
 
 const emit = defineEmits(['update'])
 
@@ -26,99 +25,10 @@ const props = defineProps({
 
 const { value, valueSaturation, hue, saturation, lightness } = props.color
 
-const lastCoords = reactive({ x: 0, y: 0 })
 const canvas = ref()
-const offscreenCanvas = Canvas.createOffscreen(512, 512)
-const isDragging = ref(false)
-
-/**
- * TODO: Rewrite this so we don't need to use all canvas resources.
- *
- * @param {*} param0
- */
-function drawOffscreenCanvas([r, g, b]) {
-  /**
-   * @type {WebGLRenderingContext}
-   */
-  const gl = offscreenCanvas.getContext('webgl')
-
-  const vertexShader = gl.createShader(gl.VERTEX_SHADER)
-  gl.shaderSource(
-    vertexShader,
-    `
-  precision highp float;
-
-  attribute vec2 a_coords;
-
-  void main() {
-    gl_Position = vec4(a_coords, 0.0, 1.0);
-  }
-  `
-  )
-  gl.compileShader(vertexShader)
-  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-    throw new Error(gl.getShaderInfoLog(vertexShader))
-  }
-
-  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
-  gl.shaderSource(
-    fragmentShader,
-    `
-  precision highp float;
-
-  uniform vec3 u_color;
-
-  void main() {
-    vec2 canvas_position = gl_FragCoord.xy / 512.0;
-    vec3 color = mix(
-      mix(
-        vec3(1.0, 1.0, 1.0),
-        u_color,
-        canvas_position.x
-      ),
-      vec3(0.0, 0.0, 0.0),
-      1.0 - canvas_position.y
-    );
-    gl_FragColor = vec4(color, 1.0);
-  }
-  `
-  )
-  gl.compileShader(fragmentShader)
-  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-    throw new Error(gl.getShaderInfoLog(fragmentShader))
-  }
-
-  const program = gl.createProgram()
-  gl.attachShader(program, vertexShader)
-  gl.attachShader(program, fragmentShader)
-  gl.linkProgram(program)
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-    throw new Error(gl.getProgramInfoLog(program))
-  }
-
-  gl.viewport(0, 0, offscreenCanvas.width, offscreenCanvas.height)
-
-  gl.useProgram(program)
-
-  const colorLocation = gl.getUniformLocation(program, 'u_color')
-  gl.uniform3f(colorLocation, r, g, b)
-
-  const vertices = new Float32Array([-1, -1, 1, -1, 1, 1, -1, 1])
-
-  const buffer = gl.createBuffer()
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
-
-  gl.enableVertexAttribArray(0)
-  gl.vertexAttribPointer(0, 2, gl.FLOAT, gl.FALSE, 0, 0)
-
-  gl.drawArrays(gl.TRIANGLE_FAN, 0, 4)
-
-  gl.deleteBuffer(buffer)
-  gl.deleteProgram(program)
-  gl.deleteShader(fragmentShader)
-  gl.deleteShader(vertexShader)
-}
+const context = computed(() => canvas.value.getContext('2d', {
+  willReadFrequently: true
+}))
 
 /*
   top: `${ -(this.props.hsv.v * 100) + 100 }%`,
@@ -130,70 +40,53 @@ const style = computed(() => ({
 }))
 
 function updateCanvas(color) {
-  drawOffscreenCanvas(color)
-  const context = canvas.value.getContext('2d', {
-    willReadFrequently: true
-  })
-  context.drawImage(
-    offscreenCanvas,
+  context.value.drawImage(
+    CombinedColorPicker.drawOffscreenCanvas(color),
     0,
     0,
-    context.canvas.width,
-    context.canvas.height
+    context.value.canvas.width,
+    context.value.canvas.height
   )
 }
 
 function updateFromPixel(x, y) {
-  const context = canvas.value.getContext('2d', {
-    willReadFrequently: true
-  })
-  const imageData = context.getImageData(x, y, 1, 1)
+  const imageData = context.value.getImageData(x, y, 1, 1)
   const [r, g, b] = imageData.data
   const extractedColor = Color.fromRGBA(r, g, b)
   saturation.value = Color.saturation(extractedColor) * 100
   lightness.value = Color.lightness(extractedColor) * 100
 }
 
+const boundingClientRect = computed(() => canvas.value.getBoundingClientRect())
+
 function getOffsetCoordinates(event, element) {
-  const source = element ?? event.currentTarget
-  const { left, top, width, height } = source.getBoundingClientRect()
+  // const source = element ?? event.currentTarget
+  const { left, top, width, height } = boundingClientRect.value // source.getBoundingClientRect()
+  const x = Math.max(0, Math.min(width - 1, event.clientX - left))
+  const y = Math.max(0, Math.min(height - 1, event.clientY - top))
   return {
-    x: Math.max(0, Math.min(width, event.clientX - left)),
-    y: Math.max(0, Math.min(height, event.clientY - top))
+    x,
+    y
   }
 }
 
 function startDragging(e) {
-  isDragging.value = true
   const { x, y } = getOffsetCoordinates(e, canvas.value)
-  lastCoords.x = x
-  lastCoords.y = y
   updateFromPixel(x, y)
+  window.addEventListener('pointermove', updateColor)
+  window.addEventListener('pointerup', stopDragging, { once: true })
+  window.addEventListener('pointerleave', stopDragging, { once: true })
 }
 
 function updateColor(e) {
-  if (!isDragging.value) return
-  if(e.pressure === 0) isDragging.value = false
-  console.log('is moving')
-  e.preventDefault()
-
   const { x, y } = getOffsetCoordinates(e, canvas.value)
-  lastCoords.x = x
-  lastCoords.y = y
   updateFromPixel(x, y)
 }
 
 function stopDragging(e) {
-  console.log('stop dragging')
-  isDragging.value = false
-}
-
-function stopDraggingOutside(e) {
-  stopDragging(e)
-  const { x, y } = getOffsetCoordinates(e, canvas.value)
-  lastCoords.x = x
-  lastCoords.y = y
-  updateFromPixel(x, y)
+  window.removeEventListener('pointermove', updateColor)
+  window.removeEventListener('pointerup', stopDragging)
+  window.removeEventListener('pointerleave', stopDragging)
 }
 
 watch(
---
+++ pixel/canvas/CombinedColorPicker.js
@@ -0,0 +1,99 @@
+import Canvas from './Canvas.js'
+
+/**
+ * Draws a color picker on an offscreen canvas.
+ *
+ * @param {[number, number, number]} color
+ * @returns {OffscreenCanvas|HTMLCanvasElement}
+ */
+export const drawOffscreenCanvas = (() => {
+  const offscreenCanvas = Canvas.createOffscreen(512, 512)
+  const gl = offscreenCanvas.getContext('webgl')
+  if (!gl) {
+    throw new Error('WebGL not supported')
+  }
+
+  const vertexShader = gl.createShader(gl.VERTEX_SHADER)
+  gl.shaderSource(
+    vertexShader,
+    `
+  precision highp float;
+
+  attribute vec2 a_coords;
+
+  void main() {
+    gl_Position = vec4(a_coords, 0.0, 1.0);
+  }
+  `
+  )
+  gl.compileShader(vertexShader)
+  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+    throw new Error(gl.getShaderInfoLog(vertexShader))
+  }
+
+  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
+  gl.shaderSource(
+    fragmentShader,
+    `
+  precision highp float;
+
+  uniform vec3 u_color;
+
+  void main() {
+    vec2 canvas_position = gl_FragCoord.xy / 512.0;
+    vec3 color = mix(
+      mix(
+        vec3(1.0, 1.0, 1.0),
+        u_color,
+        canvas_position.x
+      ),
+      vec3(0.0, 0.0, 0.0),
+      1.0 - canvas_position.y
+    );
+    gl_FragColor = vec4(color, 1.0);
+  }
+  `
+  )
+  gl.compileShader(fragmentShader)
+  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+    throw new Error(gl.getShaderInfoLog(fragmentShader))
+  }
+
+  const program = gl.createProgram()
+  gl.attachShader(program, vertexShader)
+  gl.attachShader(program, fragmentShader)
+  gl.linkProgram(program)
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    throw new Error(gl.getProgramInfoLog(program))
+  }
+
+  const vertices = new Float32Array([-1, -1, 1, -1, 1, 1, -1, 1])
+
+  const buffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
+  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
+
+  // gl.deleteBuffer(buffer)
+  // gl.deleteProgram(program)
+  // gl.deleteShader(fragmentShader)
+  // gl.deleteShader(vertexShader)
+
+  return function drawOffscreenCanvas([r, g, b]) {
+    gl.viewport(0, 0, offscreenCanvas.width, offscreenCanvas.height)
+    gl.useProgram(program)
+
+    const colorLocation = gl.getUniformLocation(program, 'u_color')
+    gl.uniform3f(colorLocation, r, g, b)
+
+    gl.enableVertexAttribArray(0)
+    gl.vertexAttribPointer(0, 2, gl.FLOAT, gl.FALSE, 0, 0)
+
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4)
+
+    return offscreenCanvas
+  }
+})()
+
+export default {
+  drawOffscreenCanvas
+}
