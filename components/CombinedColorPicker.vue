36d066816d311f9a3d29fde20d694f95c598cceb
Omar Moreno Melcón <omar@anaga.dev>
2023-10-24 22:26:56 +0200

    fix: color picker sampling

--- components/CombinedColorPicker.vue
+++ components/CombinedColorPicker.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="CombinedColorPicker">
-    <canvas ref="canvas" @pointermove="onPointer"></canvas>
-    <div class="circle" :style="style"></div>
+    <canvas
+      ref="canvas"
+      @pointerdown="startDragging"
+      @pointermove="updateColor"
+      @pointerup="stopDragging"
+      draggable="false"
+    ></canvas>
+    <div class="sample" :style="style"></div>
   </div>
 </template>
 
@@ -23,6 +29,7 @@ const { value, valueSaturation, hue, saturation, lightness } = props.color
 const lastCoords = reactive({ x: 0, y: 0 })
 const canvas = ref()
 const offscreenCanvas = Canvas.createOffscreen(512, 512)
+const isDragging = ref(false)
 
 /**
  * TODO: Rewrite this so we don't need to use all canvas resources.
@@ -36,7 +43,9 @@ function drawOffscreenCanvas([r, g, b]) {
   const gl = offscreenCanvas.getContext('webgl')
 
   const vertexShader = gl.createShader(gl.VERTEX_SHADER)
-  gl.shaderSource(vertexShader, `
+  gl.shaderSource(
+    vertexShader,
+    `
   precision highp float;
 
   attribute vec2 a_coords;
@@ -44,14 +53,17 @@ function drawOffscreenCanvas([r, g, b]) {
   void main() {
     gl_Position = vec4(a_coords, 0.0, 1.0);
   }
-  `)
+  `
+  )
   gl.compileShader(vertexShader)
   if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
     throw new Error(gl.getShaderInfoLog(vertexShader))
   }
 
   const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)
-  gl.shaderSource(fragmentShader, `
+  gl.shaderSource(
+    fragmentShader,
+    `
   precision highp float;
 
   uniform vec3 u_color;
@@ -69,7 +81,8 @@ function drawOffscreenCanvas([r, g, b]) {
     );
     gl_FragColor = vec4(color, 1.0);
   }
-  `)
+  `
+  )
   gl.compileShader(fragmentShader)
   if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
     throw new Error(gl.getShaderInfoLog(fragmentShader))
@@ -90,12 +103,7 @@ function drawOffscreenCanvas([r, g, b]) {
   const colorLocation = gl.getUniformLocation(program, 'u_color')
   gl.uniform3f(colorLocation, r, g, b)
 
-  const vertices = new Float32Array([
-    -1, -1,
-    1, -1,
-    1, 1,
-    -1, 1,
-  ])
+  const vertices = new Float32Array([-1, -1, 1, -1, 1, 1, -1, 1])
 
   const buffer = gl.createBuffer()
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
@@ -117,8 +125,8 @@ function drawOffscreenCanvas([r, g, b]) {
   left: `${ this.props.hsv.s * 100 }%`,
 */
 const style = computed(() => ({
-  top: `calc(${ -(value.value * 100) + 100 }%)`,
-  left: `${ valueSaturation.value * 100 }%`,
+  top: `calc(${-(value.value * 100) + 100}%)`,
+  left: `${valueSaturation.value * 100}%`
 }))
 
 function updateCanvas(color) {
@@ -126,7 +134,13 @@ function updateCanvas(color) {
   const context = canvas.value.getContext('2d', {
     willReadFrequently: true
   })
-  context.drawImage(offscreenCanvas, 0, 0, context.canvas.width, context.canvas.height)
+  context.drawImage(
+    offscreenCanvas,
+    0,
+    0,
+    context.canvas.width,
+    context.canvas.height
+  )
 }
 
 function updateFromPixel(x, y) {
@@ -144,22 +158,48 @@ function getOffsetCoordinates(event, element) {
   const source = element ?? event.currentTarget
   const { left, top, width, height } = source.getBoundingClientRect()
   return {
-    x: Math.max(0, Math.min(width - 3, event.clientX - left)),
-    y: Math.max(0, Math.min(height - 3, event.clientY - top))
+    x: Math.max(0, Math.min(width, event.clientX - left)),
+    y: Math.max(0, Math.min(height, event.clientY - top))
   }
 }
 
-function onPointer(e) {
-  if (!e.pressure) {
-    return
-  }
+function startDragging(e) {
+  isDragging.value = true
   const { x, y } = getOffsetCoordinates(e, canvas.value)
   lastCoords.x = x
   lastCoords.y = y
   updateFromPixel(x, y)
 }
 
-watch(() => hue.value, () => updateCanvas(Color.fromHSLA(hue.value, 100, 50, 1)))
+function updateColor(e) {
+  if (!isDragging.value) return
+  if(e.pressure === 0) isDragging.value = false
+  console.log('is moving')
+  e.preventDefault()
+
+  const { x, y } = getOffsetCoordinates(e, canvas.value)
+  lastCoords.x = x
+  lastCoords.y = y
+  updateFromPixel(x, y)
+}
+
+function stopDragging(e) {
+  console.log('stop dragging')
+  isDragging.value = false
+}
+
+function stopDraggingOutside(e) {
+  stopDragging(e)
+  const { x, y } = getOffsetCoordinates(e, canvas.value)
+  lastCoords.x = x
+  lastCoords.y = y
+  updateFromPixel(x, y)
+}
+
+watch(
+  () => hue.value,
+  () => updateCanvas(Color.fromHSLA(hue.value, 100, 50, 1))
+)
 
 onMounted(() => {
   Canvas.resize(canvas.value)
@@ -181,14 +221,12 @@ canvas {
   height: 100%;
 }
 
-.circle {
+.sample {
   position: absolute;
-  width: var(--spaceM);
-  height: var(--spaceM);
-  border-radius: 100%;
+  width: 8px;
+  height: 8px;
+  pointer-events: none;
   transform: translate(-50%, -50%);
-  box-shadow:
-    0 0 0 1px hsl(0, 0%, 100%),
-    0 0 0 2px hsl(0, 0%, 0%);
+  box-shadow: 0 0 0 2px hsl(0, 0%, 100%), 0 0 0 4px hsl(0, 0%, 0%);
 }
 </style>
