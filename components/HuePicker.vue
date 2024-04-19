bdaa232bec2c8e620f2f630001fc5ccb1d5c431d
mekanoide <omar@anaga.dev>
2024-04-19 11:01:44 +0200

    refactor: improve colorpicker performance

--- components/HuePicker.vue
+++ components/HuePicker.vue
@@ -9,21 +9,31 @@ const emit = defineEmits(['update'])
 
 const { hue, style } = props.color
 
-const computedStyle = computed(() => ({ left: `${((hue.value / 360) * 100)}%` }))
 const container = ref()
-const boundingClientRect = computed(() => container.value.getBoundingClientRect())
+const boundingClientRect = computed(() =>
+  container.value.getBoundingClientRect()
+)
+
+const computedStyle = computed(() => {
+  const containerWidth = container.value
+    ? boundingClientRect.value.width
+    : 0
+  const translateX = (hue.value / 360) * containerWidth
+  console.log('translate', hue, translateX)
+  return `transform: translateX(${translateX}px);`
+})
 
 function startDragging(e) {
-  const { width, left } = boundingClientRect.value
-  hue.value = Math.max(0, Math.min(360, Math.round((e.clientX - left) / width * 360)))
+  const { width } = boundingClientRect.value
+  hue.value = Math.max(0, Math.min(360, Math.round((e.offsetX / width) * 360)))
   window.addEventListener('pointermove', updateHue)
   window.addEventListener('pointerup', stopDragging, { once: true })
   window.addEventListener('pointerleave', stopDragging, { once: true })
 }
 
 function updateHue(e) {
-  const { width, left } = boundingClientRect.value
-  hue.value = Math.max(0, Math.min(360, Math.round((e.clientX - left) / width * 360)))
+  const { width } = boundingClientRect.value
+  hue.value = Math.max(0, Math.min(360, Math.round((e.offsetX / width) * 360)))
 }
 
 function stopDragging(e) {
@@ -52,7 +62,8 @@ function stopDragging(e) {
 
 <style scoped>
 .HuePicker {
-  background: linear-gradient(to right,
+  background: linear-gradient(
+    to right,
     hsl(0, 100%, 50%),
     hsl(30, 100%, 50%),
     hsl(60, 100%, 50%),
