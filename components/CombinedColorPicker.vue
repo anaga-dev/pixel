7d8f84d95eb5a8794d76cef43447fc02782a5649
mekanoide <omar@anaga.dev>
2024-04-22 14:22:16 +0200

    fix: redundant cursor, portrait layout, side panel expansion

--- components/CombinedColorPicker.vue
+++ components/CombinedColorPicker.vue
@@ -38,11 +38,12 @@ const boundingClientRect = computed(() => canvas.value.getBoundingClientRect())
 })) */
 
 const computedStyle = computed(() => {
-  const posX = canvas.value ? valueSaturation.value * canvas.value.width : 0
-  const posY = canvas.value ? (1 - value.value) * canvas.value.height : 0
+  const posX = canvas.value ? Math.round(valueSaturation.value * canvas.value.width) : 0
+  const posY = canvas.value ? Math.round((1 - value.value) * canvas.value.height) : 0
   console.log('Saturation', valueSaturation.value, 'Lightness', value.value)
   console.log('Pos X', posX, 'Pos Y', posY)
-  return `transform: translate(calc(${posX}px - 50%), calc(${posY}px - 50%))`
+  console.log('Offset X', offsetX, 'Offset Y', offsetY)
+  return `transform: translate(${posX}px, ${posY}px)`
 })
 
 function updateCanvas(color) {
@@ -73,12 +74,14 @@ function updateFromPixel(x, y) {
 
 function updateOffsetCoordinates(e) {
   const { left, top } = boundingClientRect.value // source.getBoundingClientRect()
-  offsetX = Math.floor(
-    Math.max(0, Math.min(canvas.value.width - 1, e.clientX - left))
+  offsetX = Math.round(
+    Math.max(0, Math.min(boundingClientRect.value.width, e.clientX - left))
   )
-  offsetY = Math.floor(
-    Math.max(0, Math.min(canvas.value.height - 1, e.clientY - top))
+  offsetY = Math.round(
+    Math.max(0, Math.min(boundingClientRect.value.height, e.clientY - top))
   )
+  console.log('Mouserrrrl', e.clientX, e.clientY)
+  console.log('Offset X', offsetX, 'Offset Y', offsetY)
 }
 
 function startDragging(e) {
@@ -137,13 +140,24 @@ canvas {
 
 .sample {
   position: absolute;
-
-  width: 0.5rem;
-  height: 0.5rem;
+  width: 0;
+  height: 0;
   pointer-events: none;
   user-select: none;
   left: 0;
   top: 0;
+}
+
+.sample::after {
+  position: absolute;
+  content: '';
+  display: block;
+  width: 0.5rem;
+  height: 0.5rem;
+  user-select: none;
+  left: 0;
+  top: 0;
+  translate: -50% -50%;
   box-shadow: 0 0 0 2px hsl(0, 0%, 100%), 0 0 0 4px hsl(0, 0%, 0%);
 }
 </style>
